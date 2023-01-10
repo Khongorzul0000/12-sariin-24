945a09d4abe6bf62be4d8b98c706a5e16a47bb8a
22PHOP2D-Khongorzul <Khongorzul@Khulangoos-iMac-2.local>
2023-01-10 18:06:58 +0800

    gg

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const { Playlist } = require("./playlistModel");
-const {Song} = require("./songModel");
-const {User} = require("./userModel");
-const {Artist} = require("./artistModel");
+const { Song } = require("./songModel");
+const { User } = require("./userModel");
+const { Artist } = require("./artistModel");
 
 module.exports = { Playlist, Song, User, Artist };
